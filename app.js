61cc7ae02d8be43cc4f2573502a0b9dff4fe7d9f
gon-alt <82797848+gon-alt@users.noreply.github.com>
2022-06-18 19:07:17 -0300

    configuracion de variable de puerto

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+
+
 //declaramos dependencias
 const express = require('express') 
+
+require('dotenv').config()
+
 // la constante app se iguala a express con sus dep
 const app = express()
 //obtenermos de app req y res
@@ -8,8 +13,11 @@ app.get('/', (req, res) => {
     console.log("peticion recibida")
     res.send('#holaMundo!')
 })
-//configuramos puerto y funcion de respuesta
-app.listen(4000, function () {
-        console.log("servidor escuchando puerto 4000")
+
+const PORT = process.env.PORT
+
+//referenciamos puerto a variable de entorno
+app.listen(PORT, () => {
+        console.log({PORT})
 
     })
